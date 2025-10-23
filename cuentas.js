6f2ab8e63ddac377a8a98f2c24341adf8ed488ea
RodrigoHernandez06 <00018024@uca.edu.sv>
2025-10-22 18:57:56 -0600

    ✨: añadiendo set de datos

---
+++ cuentas.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    "_id": "68f863f0196d296c9e219f27",
+    "isActive": true,
+    "picture": "https://files.consumerfinance.gov/f/images/protected-money_1140x642_ai_fw_io.original.png",
+    "balance": "$25223",
+    "client": "Katie Woodard",
+    "gender": "female"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f28",
+    "isActive": false,
+    "picture": "https://cdn.pixabay.com/photo/2016/08/31/11/55/money-1634984_960_720.png",
+    "balance": "$17456",
+    "client": "Michael Turner",
+    "gender": "male"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f29",
+    "isActive": true,
+    "picture": "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    "balance": "$9812",
+    "client": "Samantha Lopez",
+    "gender": "female"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f2a",
+    "isActive": true,
+    "picture": "https://cdn-icons-png.flaticon.com/512/236/236832.png",
+    "balance": "$30478",
+    "client": "James Peterson",
+    "gender": "male"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f2b",
+    "isActive": false,
+    "picture": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+    "balance": "$18750",
+    "client": "Natalie Brooks",
+    "gender": "female"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f2c",
+    "isActive": true,
+    "picture": "https://cdn-icons-png.flaticon.com/512/147/147144.png",
+    "balance": "$41290",
+    "client": "Robert Mitchell",
+    "gender": "male"
+  },
+  {
+    "_id": "68f863f0196d296c9e219f2d",
+    "isActive": false,
+    "picture": "https://cdn-icons-png.flaticon.com/512/194/194938.png",
+    "balance": "$6234",
+    "client": "Emma Johnson",
+    "gender": "female"
+  }
+]
+
+module.exports = data;
